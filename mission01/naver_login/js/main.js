679935c2446ebc309d519b0d4977e7759ac8b8a7
Moong <tmdals9154@gmail.com>
2023-07-14 02:40:42 +0900

    연습페이지 삭제

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,8 +1,7 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
 /*
 
@@ -15,95 +14,102 @@ const user = {
 
 /* -------------------------------------------------------------------------- */
 
-const submit = document.querySelector(".btn-login");           // 로그인 button                    
-const email = document.querySelector('#userEmail');            // 이메일 input
-const pwd = document.querySelector('#userPassword');           // 비밀번호 input          
+const submit = document.querySelector(".btn-login"); // 로그인 button
+const email = document.querySelector("#userEmail"); // 이메일 input
+const pw = document.querySelector("#userPassword"); // 비밀번호 input
 
 /* -------------------------------------------------------------------------- */
 
-//# 아이디(이메일) vaildation
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(text).toLowerCase())
+//@ 아이디(이메일) validation
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(text).toLowerCase());
 }
 
-//# 비밀번호 vaildation
-function pwReg(text){
+//@ 비밀번호 validation
+function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
 /* -------------------------------------------------------------------------- */
 
+//& 아이디(이메일) 에러메세지 처리
+function emailError(tag, value) {
+  if (emailReg(email.value)) {
+    // 정규식조건에 만족했을때
+    tag.classList.remove("is--invalid");
+  } else if (email.value === "") {
+    // input값이 빈공간일때
+    tag.classList.remove("is--invalid");
+  } else {
+    // 정규식조건을 만족하지 못했을때
+    tag.classList.add("is--invalid");
+  }
+}
 
-//! 에러 함수를 따로 만들었지만,
-//! addEvenetlistener 안에서 호출 되어 실행되지 않음.
-// function Error(tag, real, value) {
-//   if (real) {
-//     tag.classList.remove("is--invalid");
-//   } else if (value === "") {
-//     tag.classList.remove("is--invalid");
-//   } else {
-//     tag.classList.add("is--invalid");
-//   }
-// }
-// Error(email, realValue, value);
+//&	비밀번호 에러메세지 처리
+function pwError(tag, value) {
+  if (pwReg(pw.value)) {
+    // 정규식조건에 만족했을때
+    tag.classList.remove("is--invalid");
+  } else if (pw.value === "") {
+    // input값이 빈공간일때
+    tag.classList.remove("is--invalid");
+  } else {
+    // 정규식조건을 만족하지 못했을때
+    tag.classList.add("is--invalid");
+  }
+}
 
+//* input값 비교
+function comparison(email, pw) {
+  if (email.value !== user.id) {
+    // 아이디입력값 비교
+    alert("이메일이 올바르지 않습니다.");
+  } else if (pw.value !== user.pw) {
+    // 비밀번호값 비교
+    alert("비밀번호가 올바르지 않습니다.");
+  }
+}
 
+//^ 페이지 이동
+function move() {
+  if (email.value === user.id && pw.value === user.pw) {
+    window.location.href = "welcome.html";
+  }
+}
+
+/* -------------------------------------------------------------------------- */
 
 //# 아이디 처리 이벤트
-email.addEventListener('input', () => {
-  let value = email.value;
+email.addEventListener("input", () => {
+  //@ 정규표현식
+  let processedValue = emailReg(email.value);
 
-  //@ 정규표현식 적용
-  let realValue = emailReg(value);
-
-  //& 에러 메시지 처리
-  if (realValue) {
-    email.classList.remove("is--invalid");
-  } else if(value === '') {
-    email.classList.remove("is--invalid");
-  } else {
-    email.classList.add("is--invalid");
-  }
+  //& 에러 메시지
+  emailError(email, processedValue);
 });
-
 
 //# 비밀번호 처리 이벤트
-pwd.addEventListener("input", () => {
-  let value = pwd.value;
+pw.addEventListener("input", () => {
+  //@ 정규표현식
+  let processedValue = pwReg(pw.value);
 
-  if (value == '') {
-    pwd.classList.remove("is--invalid");
-  }
-
-  //@ 정규표현식 적용
-  let realValue = pwReg(value);
-
-  //& 에러 메시지 처리
-  if (realValue) {
-    pwd.classList.remove("is--invalid");
-  } else if (value === '') {
-    pwd.classList.remove("is--invalid");
-  } else {
-    pwd.classList.add("is--invalid");
-  }
+  //& 에러 메시지
+  pwError(pw, processedValue);
 });
 
-
 //# 로그인버튼 처리 이벤트
-submit.addEventListener('click', (event) => {
-
+submit.addEventListener("click", (event) => {
   event.preventDefault();
 
-  // 이메일값이 맞는지,
-  if (email.value !== user.id) {
-    alert('이메일이 올바르지 않습니다.')
-    // 비밀번호값이 맞는지,
-  } else if (pwd.value !== user.pw) {
-    alert("비밀번호가 올바르지 않습니다.");
-    // 맞다면 welcome 페이지로
-  } else {
-    window.location.href = 'welcome.html'
-  }
-})
+  //* input값 비교
+  comparison(email, pw);
+
+  //^ 페이지 이동
+  move();
+});
+
+/* -------------------------------------------------------------------------- */
